2a1859b6bcc33e281bc149422e81ed919d47e290
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-05-01 01:39:11 +0000

    fix: Finish

--- app/components/Finish.tsx
+++ app/components/Finish.tsx
@@ -2,7 +2,7 @@
 
 export default function Finish(){
     return(
-        <div>
+        <div className="mt-10">
             <h6>Copyright @ 2023 UTFPR. All rights reserved.</h6>
         </div>
     )
